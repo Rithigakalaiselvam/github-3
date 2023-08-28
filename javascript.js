9aa1f117e0e3a23c57bbc9a2d220c4627fdde926
Rithigakalaiselvam <143271011+Rithigakalaiselvam@users.noreply.github.com>
2023-08-28 19:25:58 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+const form=document.querySelector('#form');
+const username=document.querySelector('#username');
+const email=document.querySelector('#email');
+const password=document.querySelector('#password');
+const cpassword=document.querySelector('#cpassword');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    valifateInputs();
+
+})
+
+function  validateInputs(){
+    const usernameVal=username.value.trim();
+    const emailVal=email.value.trim();
+    const passwordVal=password.value.trim();
+    const cpasswordVal=cpassword.value.trim();
+
+}
+
+function setError(element,message){
+    const inputGroup= element.parentElement;
+    const errorElement= inputGroup.querySelector('.error')
+
+    errorElement.innerText=message;
+    inputGroup.classList.add('error')
+    inputGroup.classList.remove('success')
+}
